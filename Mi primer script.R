590a586e117fe2cfcd44e9f6c9374daa54b2dbf1
RamsesRamirez <ramsesramirea@gmail.com>
2025-10-09 19:29:27 -0600

    Mi primer script
    En este script se encuentran mis primeros comandos

---
+++ Mi primer script.R
@@ -0,0 +1,20 @@
+Hola
+#aquí va el número
+#en la ventana de abajo va a salir el resultado de nuestro análisis
+
+
+
+#Cargar matriz de datos iris en el onjeto BD que significa Base de Datos
+
+BD<-iris
+
+#Este comando sirve para visualizar una matriz de datos
+
+View(BD)
+
+#str visualiza y arroja el tipo de variables
+#hay dos tipos de variables, son cuantitativas y cualitativas
+str(iris)
+
+
+str(BD)
